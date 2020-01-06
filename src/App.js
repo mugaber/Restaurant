e892f249a07b6be527d6111b3fc8c4fa052ef7f1
Muhammed Gaber <mohammedgaber273@gmail.com>
2020-01-07 00:38:37 +0200

    finish menue

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { Component } from "react";
 // import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/Menu.js'
-import './App.css';
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/Menu.js";
+import "./App.css";
+import { DISHES } from "./shared/dishes.js";
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">
-            Ristorante Con Fusion
-          </NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={DISHES} />
+      </div>
+    );
+  }
 }
 
 export default App;
