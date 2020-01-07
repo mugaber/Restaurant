975df96264d5231bb9230618068ef32491926518
Muhammed Gaber <mohammedgaber273@gmail.com>
2020-01-08 01:41:11 +0200

    SPA 2

--- src/components/DishDetail.js
+++ src/components/DishDetail.js
@@ -1,5 +1,14 @@
 import React, { Component } from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 class DishDetail extends Component {
   constructor(props) {
@@ -55,11 +64,23 @@ class DishDetail extends Component {
       return (
         <div className="container">
           <div className="row">
+            <Breadcrumb>
+              <BreadcrumbItem>
+                <Link to="/menu">Menu</Link>
+              </BreadcrumbItem>
+              <BreadcrumbItem active>{this.props.dish.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+              <h3>{this.props.dish.name}</h3>
+              <hr />
+            </div>
+          </div>
+          <div className="row">
             <div className="col-12 col-md-5 m-1">
               {this.renderDish(this.props.dish)}
             </div>
             <div className="col-12 col-md-5 m-1">
-              {this.renderComments(this.props.dish.comments)}
+              {this.renderComments(this.props.comments)}
             </div>
           </div>
         </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import Menu from "./Menu.js";
-import { DISHES } from "../shared/dishes";
 import { PROMOTIONS } from "../shared/promotions";
 import { LEADERS } from "../shared/leaders";
 import { COMMENTS } from "../shared/comments";
+import { DISHES } from "../shared/dishes";
 import DishDetail from "./DishDetail";
+import Contact from "./Contact";
 import Header from "./Header";
 import Footer from "./Footer";
+import Menu from "./Menu.js";
 import Home from "./Home";
-import Contact from "./Contact";
 
 class Main extends Component {
   constructor(props) {
@@ -33,6 +33,21 @@ class Main extends Component {
       );
     };
 
+    const DishWithId = ({ match }) => {
+      return (
+        <DishDetail
+          dish={
+            this.state.dishes.filter(
+              dish => dish.id === parseInt(match.params.dishId, 10)
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            comment => comment.dishId === parseInt(match.params.dishId, 10)
+          )}
+        />
+      );
+    };
+
     return (
       <div>
         <Header />
@@ -44,6 +59,7 @@ class Main extends Component {
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
+          <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus">
             <Contact />
           </Route>
