84f33d8dec45fcc4e69615e56a04b3843ae44ec9
Muhammed Gaber <mohammedgaber273@gmail.com>
2020-01-11 15:51:41 +0200

    fetch and action creator

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,4 +1,5 @@
 import * as ActionTypes from "./ActionTypes";
+import { baseUrl } from "../shared/baseUrl";
 import { DISHES } from "../shared/dishes";
 
 export const addComment = (dishId, rating, author, comment) => ({
@@ -14,9 +15,17 @@ export const addComment = (dishId, rating, author, comment) => ({
 export const fetchDishes = () => dispatch => {
   dispatch(dishesLoading(true));
 
-  setTimeout(() => {
-    dispatch(addDishes(DISHES));
-  }, 2000);
+  // to fetch data from the json server and serve up the dishes
+  // we have to include the url to the fetch API and make up
+  // the promise by using then and handle the error by catch
+  // the first then will transfer the response to a json object
+  // then the json object get passed to the second then to be
+  // passed to the addDishes function that the dispatch will
+  // make it available to the redux store
+
+  return fetch(baseUrl + "dishes")
+    .then(response => response.json())
+    .then(dishes => dispatch(addDishes(dishes)));
 };
 
 export const dishesLoading = () => ({
@@ -31,4 +40,26 @@ export const dishesFailed = errmessg => ({
 export const addDishes = dishes => ({
   type: ActionTypes.ADD_DISHES,
   payload: dishes
+});
+
+export const fetchPromos = () => dispatch => {
+  dispatch(promosLoading());
+
+  return fetch(baseUrl + "promotions")
+    .then(response => response.json())
+    .then(promos => dispatch(addDishes(promos)));
+};
+
+export const promosLoading = () => ({
+  type: ActionTypes.PROMOS_LOADING
+});
+
+export const promosFailed = errmess => ({
+  type: ActionTypes.PROMOS_FAILED,
+  payload: errmess
+});
+
+export const addPromos = promos => ({
+  type: ActionTypes.ADD_PROMOS,
+  payload: promos
 });
