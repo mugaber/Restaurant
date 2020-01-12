d9acf3012cd665e147c7e4298032dbdd40de9560
Muhammed Gaber <mohammedgaber273@gmail.com>
2020-01-12 03:36:33 +0200

    fetch handling errors in ActionCreators

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -14,9 +14,34 @@ export const addComment = (dishId, rating, author, comment) => ({
 // COMMENTS ACTIONS
 
 export const fetchComments = () => dispatch => {
-  return fetch(baseUrl + "comments")
-    .then(response => response.json())
-    .then(comments => dispatch(addComments(comments)));
+  return (
+    fetch(baseUrl + "comments")
+      // to handle the situration if there is an error
+      // first we create an error from the response then
+      // throw it to create and error message from it
+      // in the same then situation then we will catch
+      // the error message that have been created and thrown
+      .then(
+        response => {
+          if (response.ok) {
+            return response;
+          } else {
+            var error = new Error(
+              "Error " + response.status + ": " + response.statusText
+            );
+            error.response = response;
+            throw error;
+          }
+        },
+        error => {
+          var errmess = new Error(error.message);
+          throw errmess;
+        }
+      )
+      .then(response => response.json())
+      .then(comments => dispatch(addComments(comments)))
+      .catch(error => dispatch(commentsFailed(error.message)))
+  );
 };
 
 export const commentsFailed = errmess => ({
@@ -35,17 +60,35 @@ export const fetchDishes = () => dispatch => {
   dispatch(dishesLoading(true));
 
   return fetch(baseUrl + "dishes")
+    .then(
+      response => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error(
+            "Error " + response.status + ": " + response.statusText
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      error => {
+        var errmess = new Error(error.message);
+        throw errmess;
+      }
+    )
     .then(response => response.json())
-    .then(dishes => dispatch(addDishes(dishes)));
+    .then(dishes => dispatch(addDishes(dishes)))
+    .catch(error => dispatch(dishesFailed(error.message)));
 };
 
 export const dishesLoading = () => ({
   type: ActionTypes.DISHES_LOADING
 });
 
-export const dishesFailed = errmessg => ({
+export const dishesFailed = errmess => ({
   type: ActionTypes.DISHES_FAILED,
-  payload: errmessg
+  payload: errmess
 });
 
 export const addDishes = dishes => ({
@@ -59,9 +102,53 @@ export const fetchPromos = () => dispatch => {
   dispatch(promosLoading());
 
   return fetch(baseUrl + "promotions")
+    .then(
+      response => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error(
+            "Error " + response.status + ": " + response.statusText
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      error => {
+        var errmess = new Error(error.message);
+        throw errmess;
+      }
+    )
     .then(response => response.json())
-    .then(promos => dispatch(addPromos(promos)));
+    .then(promos => dispatch(addPromos(promos)))
+    .catch(error => dispatch(promosFailed(error.message)));
 };
+
+// export const fetchPromos = () => dispatch => {
+//   dispatch(promosLoading());
+
+//   return fetch(baseUrl + "promotions")
+//     .then(
+//       response => {
+//         if (response.ok) {
+//           return response;
+//         } else {
+//           var error = new Error(
+//             "Error " + response.status + ": " + response.statusText
+//           );
+//           error.response = response;
+//           throw error;
+//         }
+//       },
+//       error => {
+//         var errmess = new Error(error.message);
+//         throw errmess;
+//       }
+//     )
+//     .then(response => response.json())
+//     .then(promos => dispatch(addPromos(promos)));
+//     .catch(error => dispatch(promosFailed(error.message)));
+// };
 
 export const promosLoading = () => ({
   type: ActionTypes.PROMOS_LOADING
